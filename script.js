603418a52380a73c41bc7eca1c4b15340a24a7bc
rafi-it-tech <rafie6195@gmail.com>
2023-10-19 00:50:23 +0700

    landing page update1

--- script.js
+++ script.js
@@ -29,3 +29,91 @@ signupLoginLink.forEach(link => {
         formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
     });
 });
+
+// Login
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+    const successMessage = document.querySelector(".success-message");
+    const errorMessage = document.querySelector(".error-message");
+
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const emailInput = document.getElementById("email");
+      const passwordInput = document.getElementById("password");
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      // Kirim permintaan ke mock API
+      fetch("https://652a45e54791d884f1fcb765.mockapi.io/customer")
+        .then((response) => response.json())
+        .then((data) => {
+     
+          // Loop melalui data untuk mencari kecocokan email dan password
+          let userFound = false;
+          data.forEach(function (user) {
+            console.log(user);
+            if (user.email === email && user.password === password) {
+              userFound = true;
+              // Jika kecocokan ditemukan, tampilkan pesan sukses
+              // successMessage.style.display = "block";
+              // alert(`Selamat Datang ${user.nama} di aplikasi Hetrik`);
+
+              localStorage.setItem("emailLogin", user.email);
+              localStorage.setItem("namaCustomer", user.nama);
+
+              localStorage.setItem("passwordLogin", user.password);
+
+            //   localStorage.getItem("lastname");
+            window.location.href = "landing_page.html?login=yes";
+        
+        }
+          });
+
+          // Jika tidak ada kecocokan, tampilkan pesan kesalahan
+          if (!userFound) {
+            errorMessage.style.display = "block";
+          }
+        });
+    });
+  });
+
+
+
+//   Registrasi
+  document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("emails").value;
+    const password = document.getElementById("passwords").value;
+    
+    // console.log(email);
+    // Data pendaftaran
+    const data = {
+        nama: nama,
+        email: email,
+        password: password
+    };
+    
+    // Kirim data ke API Mock
+    fetch("https://652a45e54791d884f1fcb765.mockapi.io/customer", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        localStorage.setItem("emailLogin", data.email);
+              localStorage.setItem("namaCustomer", data.nama);
+
+              localStorage.setItem("passwordLogin", data.password);
+        window.location.href = "landing_page.html?login=yes";
+    })
+    .catch(error => {
+        console.error("Terjadi kesalahan: " + error);
+    });
+});
